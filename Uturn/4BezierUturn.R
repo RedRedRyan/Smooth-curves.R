4a090cab7ee9c7536adad938529e8268c12deb1d
RedRedRyan <ryankipkorir2002@gmail.com>
2025-04-25 13:52:55 +0300

    turns

---
+++ Uturn/4BezierUturn.R
@@ -0,0 +1,20 @@
+# Install and load required packages
+library(Hmisc)
+
+# Original U-turn path data
+X <- c(0, 0, 4, 4)  
+Z <- c(0, 2, 2, 0)  
+
+# Generate Bézier curve with 100 points
+bezier_curve <- bezier(X, Z, evaluation = 20)
+
+# Create the plot
+plot(bezier_curve, type = "l", col = "blue", lwd = 3,
+     xlab = "X (Distance)", ylab = "Z (Position)", 
+     main = "4 point U-Turn Path (Bézier Curve)",
+     xlim = range(X), ylim = range(Z))
+
+# Add original control points
+points(X, Z, pch = 19, col = "red", cex = 1.2)
+grid()
+
--- Uturn/Uturn.R
+++ Uturn/Uturn.R
@@ -1,8 +1,8 @@
 
 
 # Line graph that represents a U-turn
-X <- c(0, 0, 0.25, 1, 2, 3, 3.75, 4, 4)  # Example X values
-Z <- c(0, 2, 3, 3.75, 4, 3.75, 3, 2, 0)  # Example Z values
+X <- c(0, 0, 4, 4)  
+Z <- c(0, 2, 2, 0)
 
 plot(X, Z, type = "l", col = "blue", lwd = 2,
      xlab = "X", ylab = "Z", main = "Line Graph of Z against X")
